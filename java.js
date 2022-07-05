cb6afd699e4568d350daa032efb1b0d3d9223fec
BlarQ <collinsogunlala@gmail.com>
2022-07-05 13:29:36 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,46 @@
+const ntells = "50years"
+document.write(ntells)
+
+const mo = 8;
+const no = 7;
+console.log(mo + no);
+console.log(mo * no);
+
+if (no === 3) {
+    console.log(true)
+} else {
+    console.log(false);
+}
+
+const car = {
+    color: "red",
+    model: "honda",
+    wheel: "four",
+    year: 2005,
+    new: "false",
+    seat: "4 seats",
+    tinted: "true"
+};
+
+car.hp = true;
+
+console.log(car)
+
+delete car.model;
+
+console.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
